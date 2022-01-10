0ab511286e528140e37c0fae51555553efa18baf
Jackson kanchanapally <92465955+kanchanapallyjackson@users.noreply.github.com>
2022-01-10 13:36:12 +0530

    projects

---
+++ calculator/index.js
@@ -0,0 +1,40 @@
+let screen=document.getElementById('screen');
+let buttons=document.querySelectorAll('button');
+let screenValue='';
+for(item of buttons)
+{
+    item.addEventListener('click',(e)=>
+    {
+        buttonText=e.target.innerText;
+        console.log("btn is ",buttonText)
+        if(buttonText=='X')
+        {
+            buttonText='*';
+            screenValue+=buttonText;
+            screen.value=screenValue;
+        }
+        else if(buttonText=='=')
+        {
+            screen.value=eval(screenValue);
+        }
+        else if(buttonText=='del')
+        {
+            screen.value=screen.value.slice(0,-1);
+            screenValue=screen.value
+        }
+        else if(buttonText=='CE')
+        {
+            screen.value='';
+            screenValue='';
+        }
+        else if(buttonText=='√')
+        {
+            screen.value=Math.sqrt(screenValue);
+        }
+        else{
+            screenValue+=buttonText;
+            screen.value=screenValue;
+        }
+
+    })
+}
